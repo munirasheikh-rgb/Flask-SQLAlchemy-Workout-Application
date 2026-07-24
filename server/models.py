23ced531bac5eb95ceb6c72c55175e5af2726825
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-07-24 20:08:17 +0300

    added workout_exercise model

--- server/models.py
+++ server/models.py
@@ -14,7 +14,7 @@ class Exercise(db.Model):
     category= db.column(db.String,nullable=False)
     equipment_needed = db.column(db.Boolean,nullable=False,default=False)
 
-    workout_exercises = db.relationship("WorkoutExercise",back_populates="exercise",cascade="all ,delete_orphan")
+    workout_exercises = db.relationship("WorkoutExercise",back_populates="exercises",cascade="all ,delete_orphan")
 
 class Workout(db.Model):
     __tablename__="workouts"
@@ -23,7 +23,16 @@ class Workout(db.Model):
     duration_minutes = db.column(db.Integer,nullable=False)
     notes = db.column(db.String)
 
-    workout_exercises = db.relationship("WorkoutExercise",back_populates="workout",cascade="all ,delete_orphan")
+    workout_exercises = db.relationship("WorkoutExercise",back_populates="workouts",cascade="all ,delete_orphan")
 
+class WorkoutExercise(db.Model):
+    __tablename__="workout_exercises"
+    id = db.column(db.Integer,primary_key=True)
+    workout_id = db.column(db.Integer,db.ForeignKey("workouts.id"),nullable=False)
+    exercise_id = db.column(db.Integer,db.ForeignKey("exercises.id"),nullable=False)
+    reps = db.column(db.Integer)
+    sets = db.column(db.Integer)
+    duration_seconds = db.column(db.Integer)
 
-    
+    workout = db.relatioship("Workout",back_populates="workout_exercises")
+    exercise = db.relatioship("exercise",back_populates="workout_exercises")
