26a242b3e837f60ce46034a41217d034b9917d86
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-07-25 15:53:51 +0300

    Insert workout-exercise seed data

--- server/seed.py
+++ server/seed.py
@@ -24,3 +24,12 @@ with app.app_context():
 
     db.session.add_all([workout1,workout2,workout3])
     db.session.commit()
+
+
+    workout_exercise1 = WorkoutExercise(workout_id= workout1.id,exercise_id=exercise2.id,reps=8,sets=4,duration_seconds=1000)
+    workout_exercise2 = WorkoutExercise(workout_id= workout2.id,exercise_id=exercise3.id,reps=6,sets=2,duration_seconds=1500)
+    workout_exercise3= WorkoutExercise(workout_id= workout3.id,exercise_id=exercise4.id,duration_seconds=1000)
+    workout_exercise4 = WorkoutExercise(workout_id= workout2.id,exercise_id=exercise1.id,reps=11,duration_seconds=3000)
+
+    db.session.add_all([workout_exercise1,workout_exercise2,workout_exercise3,workout_exercise4])
+    db.session.commit()
