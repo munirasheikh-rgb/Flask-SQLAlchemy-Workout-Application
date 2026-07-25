ce22eb990514bf033e21e3265f940300b86f0f9e
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-07-25 16:13:23 +0300

    Complete seed data for Workout tracker

--- server/seed.py
+++ server/seed.py
@@ -3,12 +3,12 @@ from models import db, Exercise,Workout, WorkoutExercise
 from datetime import date
 
 with app.app_context():
-
+# clear old data to prevent duplicates
     WorkoutExercise.query.delete()
     Workout.query.delete()
     Exercise.query.delete()
     db.session.commit()
-
+# seed exercise table
     exercise1 = Exercise(name="push-ups",category="balance",equipment_needed=False)
     exercise2 = Exercise(name="Tread_mill running",category="cardio",equipment_needed=True)
     exercise3 = Exercise(name="leg stretches",category="pilates",equipment_needed=False)
@@ -17,7 +17,7 @@ with app.app_context():
     db.session.add_all([exercise1,exercise2,exercise3,exercise4])
     db.session.commit()
 
-
+# seed workout table
     workout1 = Workout(date=date(2026,7,20),duration_minutes=30,notes="Cardio and lower body session")
     workout2 =  Workout(date=date(2026,7,22),duration_minutes=45,notes="Upper-body and glutes session")
     workout3 = Workout(date=date(2026,7,26),duration_minutes=20,notes="Core and yoga sessions")
@@ -25,7 +25,7 @@ with app.app_context():
     db.session.add_all([workout1,workout2,workout3])
     db.session.commit()
 
-
+# seed workout_exercise table
     workout_exercise1 = WorkoutExercise(workout_id= workout1.id,exercise_id=exercise2.id,reps=8,sets=4,duration_seconds=1000)
     workout_exercise2 = WorkoutExercise(workout_id= workout2.id,exercise_id=exercise3.id,reps=6,sets=2,duration_seconds=1500)
     workout_exercise3= WorkoutExercise(workout_id= workout3.id,exercise_id=exercise4.id,duration_seconds=1000)
@@ -33,3 +33,5 @@ with app.app_context():
 
     db.session.add_all([workout_exercise1,workout_exercise2,workout_exercise3,workout_exercise4])
     db.session.commit()
+
+    print ("Database seeded with data successfully!")
