03111af5a9280a1a44b48a9dd3d09a0917e6892e
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-07-26 01:04:29 +0300

    Add route to get a specific exercise and related workouts

--- server/app.py
+++ server/app.py
@@ -29,6 +29,33 @@ def view_exercises():
     }for exercise in exercises]),200
 
 
+@app.route("/exercises/<int:id>",methods=["GET"])
+def get_exercise_and_workouts(id):
+    exercise = db.session.get(Exercise,id)
+    if exercise is None:
+        return jsonify({
+            'error':'Exercise not found'
+        }),404
+    else:
+      return jsonify({
+          "id":exercise.id,
+          "name":exercise.name,
+          "category":exercise.category,
+          "equipment_needed":exercise.equipment_needed,
+
+          "workouts":[{
+              "id":we.workout.id,
+              "date":we.workout.date,
+              "notes":we.workout.notes,
+              "sets":we.sets,
+              "reps":we.reps,
+              "duration_seconds":we.duration_seconds
+
+          }for we in exercise.workout_exercises]
+      }),200
+    
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
