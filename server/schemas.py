416311c900070c45ad0de4b8c028aaec3ee984f0
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-07-27 17:04:14 +0300

    Create exercise,workout and workout-exercise Schemas

--- server/schemas.py
+++ server/schemas.py
@@ -1,5 +1,25 @@
 from flask_marshmallow import Marshmallow
-from marshmallow import fields ,validate
+from marshmallow import fields ,Schema
 
 ma = Marshmallow()
 
+class ExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    category =fields.Str(required=True)
+    equipment_needed = fields.Bool(load_default=False)
+
+class WorkoutSchema(Schema):
+    id = fields.Int(dump_only=True)
+    date = fields.Date(required=True)
+    duration_minutes =fields.Int(required=True)
+    notes = fields.Str(allow_none=True)
+    
+
+class WorkoutExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    workout_id = fields.Int(required=True)
+    exercise_id =fields.Int(required=True)
+    reps = fields.Int(allow_none=True)
+    sets=fields.Int(allow_none=True)
+    duration__seconds=fields.Int(allow_none=True)
